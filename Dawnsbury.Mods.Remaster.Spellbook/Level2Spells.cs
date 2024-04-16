a4da22b04158afde0f025352124577e5670c2c73
Robin McCollum <robin.mccollum@gmail.com>
2024-04-15 17:57:54 -0700

    Added Calm, Laughing Fit, Mist, and Stupefy Added Slide to Acid Grip

--- Dawnsbury.Mods.Remaster.Spellbook/Level2Spells.cs
+++ Dawnsbury.Mods.Remaster.Spellbook/Level2Spells.cs
@@ -15,6 +15,11 @@ using Dawnsbury.Display.Text;
 using Dawnsbury.Core.Mechanics.Core;
 using Dawnsbury.Core.Roller;
 using Dawnsbury.Core.Mechanics;
+using Dawnsbury.Core.Mechanics.Targeting.TargetingRequirements;
+using Dawnsbury.Core.Tiles;
+using System.Collections.Generic;
+using System.Linq;
+using Dawnsbury.Display.Illustrations;
 
 namespace Dawnsbury.Mods.Remaster.Spellbook
 {
@@ -59,10 +64,8 @@ namespace Dawnsbury.Mods.Remaster.Spellbook
         // * Illusory Creature
         // * Share Life
         // * Sound Body
-
         public static void RegisterSpells()
         {
-            // Acid Arrow => Acid Grip
             // Renamed from Acid Arrow. Updated traits, description, and functionality
             ModManager.RegisterNewSpell("AcidGrip", 2, ((spellId, spellcaster, spellLevel, inCombat, spellInformation) =>
             {
@@ -90,8 +93,7 @@ namespace Dawnsbury.Mods.Remaster.Spellbook
                     int moveDistance = checkResult switch { CheckResult.Success => 1, CheckResult.Failure => 2, CheckResult.CriticalFailure => 4, _ => 0 };
                     if (moveDistance > 0)
                     {
-                        // FIXME: should be able to move any direction
-                        await caster.PushCreature(target, moveDistance);
+                        await CommonSpellEffects.Slide(caster, target, moveDistance);
                     }
                 });
             }));
@@ -140,12 +142,156 @@ namespace Dawnsbury.Mods.Remaster.Spellbook
                 });
             }));
 
-            // Calm (formerly Calm Emotions)            
+            // Calm (formerly Calm Emotions)
+            ModManager.RegisterNewSpell("Calm", 2, ((spellId, spellcaster, spellLevel, inCombat, spellInformation) =>
+            {
+                return Spells.CreateModern(IllustrationName.CalmEmotions, "Calm", new[] { Trait.Concentrate, Trait.Emotion, Trait.Incapacitation, Trait.Manipulate, Trait.Mental, Trait.Divine, Trait.Occult, RemasterSpells.Trait.Remaster },
+                    "You forcibly calm creatures in the area, soothing them into a nonviolent state; each creature must attempt a Will save.",
+                    S.FourDegreesOfSuccess("The creature is unaffected.", "Calming urges impose a –1 status penalty to the creature's attack rolls.",
+                        "Any emotion effects that would affect the creature are suppressed and the creature can't use hostile actions. If the target is subject to hostility from any other creature, it ceases to be affected by {i}calm{/i}.", "As failure, but hostility doesn't end the effect."),
+                        Target.Burst(24, 2), spellLevel, SpellSavingThrow.Standard(Defense.Will)).WithSoundEffect(SfxName.PureEnergyRelease)
+                .WithEffectOnEachTarget(async delegate (CombatAction spell, Creature caster, Creature target, CheckResult result)
+                {
+                    QEffect qEffect;
+                    switch (result)
+                    {
+                        default:
+                            return;
+                        case CheckResult.Success:
+                            qEffect = new QEffect("Calm", "You get a -1 status penalty to attack rolls.", ExpirationCondition.ExpiresAtEndOfSourcesTurn, caster, IllustrationName.CalmEmotions)
+                            {
+                                BonusToAttackRolls = (QEffect effect, CombatAction action, Creature? arg3) => new Bonus(-1, BonusType.Status, "Calm")
+                            };
+                            break;
+                        case CheckResult.Failure:
+                            qEffect = new QEffect("Calm", "You can't use hostile actions. If you're attacked by any other creature, the spell ends on you.", ExpirationCondition.ExpiresAtEndOfSourcesTurn, caster, IllustrationName.CalmEmotions)
+                            {
+                                Id = QEffectId.CalmEmotions,
+                                PreventTakingAction = (CombatAction action) => (!action.WillBecomeHostileAction) ? null : "You can't use hostile actions."
+                            };
+                            qEffect.AddGrantingOfTechnical((Creature cr) => cr.EnemyOf(target), delegate (QEffect qfTechnical)
+                            {
+                                qfTechnical.AfterYouTakeHostileAction = delegate (QEffect qfTechnical2, CombatAction hostileAction)
+                                {
+                                    if (hostileAction.ChosenTargets.Targets(target))
+                                    {
+                                        qEffect.ExpiresAt = ExpirationCondition.Immediately;
+                                    }
+                                };
+                            });
+                            break;
+                        case CheckResult.CriticalFailure:
+                            qEffect = new QEffect("Calm", "You can't use hostile actions.", ExpirationCondition.ExpiresAtEndOfSourcesTurn, caster, IllustrationName.CalmEmotions)
+                            {
+                                Id = QEffectId.CalmEmotions,
+                                PreventTakingAction = (CombatAction action) => (!action.WillBecomeHostileAction) ? null : "You can't use hostile actions."
+                            };
+                            break;
+                    }
+
+                    qEffect.CannotExpireThisTurn = true;
+                    target.AddQEffect(qEffect);
+                    QEffect? qSustainMultipleEffect = caster.QEffects.FirstOrDefault((QEffect qfc) => qfc.Id == QEffectId.SustainingMultiple && qfc.SourceAction == spell);
+                    if (qSustainMultipleEffect == null)
+                    {
+                        qSustainMultipleEffect = QEffect.SustainingMultiple(spell);
+                        caster.AddQEffect(qSustainMultipleEffect);
+                    }
+
+                    List<QEffect> list = (List<QEffect>)qSustainMultipleEffect.Tag;
+                    list.Add(qEffect);
+                });
+            }));
+
             // False Vitality (handle like Mystic Armor)
             // Floating Flame (formerly Flaming Sphere)
+
             // Laughing Fit (formerly Hideous Laughter)
+            ModManager.RegisterNewSpell("LaughingFit", 2, ((spellId, spellcaster, spellLevel, inCombat, spellInformation) =>
+            {
+                return Spells.CreateModern(IllustrationName.HideousLaughter, "Laughing Fit", new[] { Trait.Concentrate, Trait.Emotion, Trait.Manipulate, Trait.Mental, Trait.Arcane, Trait.Occult, RemasterSpells.Trait.Remaster },
+                    "The target is overtaken with uncontrollable laughter.",
+                    "It must attempt a Will save." +
+                    S.FourDegreesOfSuccess("The target is unaffected.", "The target is plagued with uncontrollable laughter. It can't use reactions.",
+                        "The target is slowed 1 and can't use reactions.", "The target falls prone and can't use actions or reactions for 1 round. It then takes the effects of a failure."),
+                    Target.Ranged(6).WithAdditionalConditionOnTargetCreature(new LivingCreatureTargetingRequirement()), spellLevel, SpellSavingThrow.Standard(Defense.Will))
+                .WithSoundEffect(SfxName.HideousLaughterVoiceMaleB02)
+                .WithEffectOnEachTarget(async delegate (CombatAction spell, Creature caster, Creature target, CheckResult checkResult)
+                {
+                    if (checkResult <= CheckResult.Success)
+                    {
+                        if (checkResult == CheckResult.CriticalFailure)
+                        {
+                            await target.FallProne();
+                        }
+
+                        QEffect qEffect = new QEffect("Laughing Fit", "You can't take reactions.", ExpirationCondition.ExpiresAtEndOfSourcesTurn, caster, IllustrationName.HideousLaughter)
+                        {
+                            CannotExpireThisTurn = true,
+                            StateCheck = delegate (QEffect qLaughingEffect)
+                            {
+                                qLaughingEffect.Owner.AddQEffect(new QEffect(ExpirationCondition.Ephemeral)
+                                {
+                                    Id = QEffectId.CannotTakeReactions
+                                });
+                                if (checkResult <= CheckResult.Failure && !qLaughingEffect.Owner.HasEffect(QEffectId.Slowed))
+                                {
+                                    qLaughingEffect.Owner.AddQEffect(QEffect.Slowed(1).WithExpirationEphemeral());
+                                }
+                            }
+                        };
+                        if (checkResult == CheckResult.CriticalFailure)
+                        {
+                            target.AddQEffect(new QEffect("Laughing Fit (critical failure)", "You can't take actions.", ExpirationCondition.ExpiresAtStartOfSourcesTurn, caster, IllustrationName.HideousLaughter)
+                            {
+                                PreventTakingAction = (CombatAction action) => (action.ActionId == ActionId.EndTurn) ? null : "You can't take actions due to Laughing Fit."
+                            });
+                        }
+
+                        target.AddQEffect(qEffect);
+                        caster.AddQEffect(QEffect.Sustaining(spell, qEffect));
+                    }
+                });
+            }));
+
             // Mist (formerly Obscuring Mist)
-            
+            ModManager.RegisterNewSpell("Mist", 2, ((spellId, spellcaster, spellLevel, inCombat, spellInformation) =>
+            {
+                return Spells.CreateModern(IllustrationName.ObscuringMist, "Mist", new[] { Trait.Concentrate, Trait.Manipulate, Trait.Water, Trait.Arcane, Trait.Primal, RemasterSpells.Trait.Remaster },
+                    "You call forth a cloud of mist.",
+                    "All creatures within the mist become concealed, and all creatures outside the mist become concealed to creatures within it. You can Dismiss the cloud." + "\n\n{i}Dismiss Mist is in Other maneuvers menu.{/i}",
+                    Target.Burst(24, 4), spellLevel, null).WithSoundEffect(SfxName.GaleBlast).WithActionCost(3)
+                .WithEffectOnChosenTargets(async delegate (Creature caster, ChosenTargets targets)
+                {
+                    List<TileQEffect> effects = new List<TileQEffect>();
+                    foreach (Tile tile in targets.ChosenTiles)
+                    {
+                        TileQEffect item = new TileQEffect(tile)
+                        {
+                            StateCheck = delegate
+                            {
+                                tile.FoggyTerrain = true;
+                            },
+                            Illustration = IllustrationName.Fog,
+                            ExpiresAt = ExpirationCondition.Never
+                        };
+                        effects.Add(item);
+                        tile.QEffects.Add(item);
+                    }
+
+                    caster.AddQEffect(new QEffect
+                    {
+                        ProvideActionIntoPossibilitySection = (QEffect effect, PossibilitySection section) => (section.PossibilitySectionId != PossibilitySectionId.OtherManeuvers) ? null : new ActionPossibility(new CombatAction(caster, IllustrationName.ObscuringMist, "Dismiss Mist", new[] { Trait.Concentrate }, "Dismiss this effect.", Target.Self()).WithEffectOnSelf(delegate
+                        {
+                            foreach (TileQEffect tileEffect in effects)
+                            {
+                                tileEffect.ExpiresAt = ExpirationCondition.Immediately;
+                            }
+                        }))
+                    });
+                });
+            }));
+
             // Noise Blast (formerly Sound Burst)
             ModManager.RegisterNewSpell("NoiseBlast", 2, ((spellId, spellcaster, spellLevel, inCombat, spellInformation) =>
             {
@@ -177,7 +323,7 @@ namespace Dawnsbury.Mods.Remaster.Spellbook
                     "The target's skin becomes tough, with a consistency like bark or wood.",
                     "The target gains resistance 2 to bludgeoning and piercing damage and weakness 3 to fire. After the target takes fire damage, it can Dismiss the spell as a free action triggered by taking the damage; doing so doesn't reduce the fire damage the target was dealt.", 
                     Target.AdjacentFriendOrSelf((Target tg, Creature a, Creature d) => (a == d && !a.HasEffect(QEffectId.Barkskin) && !a.HasEffect(QEffectId.EndedBarkskin)) ? 15 : int.MinValue), spellLevel, null).WithSoundEffect(SfxName.ArmorDon)
-                .WithEffectOnEachTarget(async delegate (CombatAction spell, Creature caster, Creature target, CheckResult result)
+                .WithEffectOnEachTarget(async delegate (CombatAction spell, Creature caster, Creature target, CheckResult checkResult)
                 {
                     QEffect updatedEffect = QEffect.Barkskin();
                     updatedEffect.Name = "Oaken Resilience";
@@ -187,8 +333,50 @@ namespace Dawnsbury.Mods.Remaster.Spellbook
 
             // Revealing Light (formerly Faerie Fire)
             // See the Unseen (formerly See Invisible)
+
             // Spiritual Armament (formerly Spiritual Weapon)
+            //ModManager.RegisterNewSpell("SpiritualArmament", 2, ((spellId, spellcaster, spellLevel, inCombat, spellInformation) =>
+            //{
+            //    return Spells.CreateModern(IllustrationName.SpiritualWeapon, "Spiritual Armament", new[] { Trait.Concentrate, Trait.Manipulate, RemasterSpells.Trait.Sanctified, RemasterSpells.Trait.Spirit, Trait.Divine, Trait.Occult, RemasterSpells.Trait.Remaster },
+            //        "You create a ghostly, magical echo of one weapon you're wielding or wearing and fling it.",
+            //        "Attempt a spell attack roll against the target's AC, dealing 2d8 damage on a hit (or double damage on a critical hit). The damage type is the same as the chosen weapon (or any of its types for a versatile weapon). The attack deals spirit damage instead if that would be more detrimental to the creature (as determined by the GM). This attack uses and contributes to your multiple attack penalty. After the attack, the weapon returns to your side. If you sanctify the spell, the attacks are sanctified as well.",
+            //        Target.Ranged(24), spellLevel, null).WithSpellAttackRoll().WithSoundEffect(SfxName.PureEnergyRelease)
+            //    .WithProjectileCone(IllustrationName.SpiritualWeapon, 0, ProjectileKind.None)
+            //    .WithEffectOnEachTarget(async delegate (CombatAction spell, Creature caster, Creature target, CheckResult checkResult)
+            //    {
+            //        Action<QEffect> attackAction = (qEffect) =>
+            //        {
+
+            //        };
+            //        QEffect qEffect = new QEffect("Spiritual Armament", "Sustaining Spiritual Armament", ExpirationCondition.ExpiresAtStartOfSourcesTurn, caster, IllustrationName.SpiritualWeapon);
+            //        //caster.AddQEffect(QEffect.Sustaining(spell, effect, attackAction());
+            //    });
+            //}));
+
             // Stupefy (formerly Touch of Idiocy)
+            ModManager.RegisterNewSpell("Stupefy", 2, ((spellId, spellcaster, spellLevel, inCombat, spellInformation) =>
+            {
+                return Spells.CreateModern(IllustrationName.TouchOfIdiocy, "Stupefy", new[] { Trait.Concentrate, Trait.Manipulate, Trait.Mental, Trait.Arcane, Trait.Occult, RemasterSpells.Trait.Remaster },
+                    "You dull the target's mind, depending on its Will save.", 
+                    S.FourDegreesOfSuccess("The target is unaffected.", "The target is stupefied 1 until the start of your next turn.", "The target is stupefied 2 for 1 minute.", "The target is stupefied 3 for 1 minute."),
+                    Target.Ranged(6), spellLevel, SpellSavingThrow.Standard(Defense.Will)).WithSoundEffect(SfxName.Mental)
+                .WithEffectOnEachTarget(async delegate (CombatAction spell, Creature caster, Creature target, CheckResult checkResult)
+                {
+                    switch (checkResult) {
+                        case CheckResult.CriticalSuccess:
+                            break;
+                        case CheckResult.Success:
+                            target.AddQEffect(QEffect.Stupefied(1).WithExpirationAtStartOfSourcesTurn(caster, 1));
+                            break;
+                        case CheckResult.Failure:
+                            target.AddQEffect(QEffect.Stupefied(2).WithExpirationNever());
+                            break;
+                        case CheckResult.CriticalFailure:
+                            target.AddQEffect(QEffect.Stupefied(3).WithExpirationNever());
+                            break;
+                    }
+                });
+            }));
         }
     }
 }
