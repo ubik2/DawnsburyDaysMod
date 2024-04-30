a7bf679ca11998f39b80ffd61ab602c345a41a1f
Robin McCollum <robin.mccollum@gmail.com>
2024-04-30 03:49:38 -0700

    Use the new trait to hide the legacy spells in character creation. Updated Known Issues Use ModManager.RegisterActionOnEachSpell instead of ModManager.ReplaceExistingSpell.

--- Dawnsbury.Mods.Remaster.HideLegacySpells/HideLegacySpells.cs
+++ Dawnsbury.Mods.Remaster.HideLegacySpells/HideLegacySpells.cs
@@ -1,12 +1,7 @@
-﻿using Dawnsbury.Core.CharacterBuilder.FeatsDb.Spellbook;
-using Dawnsbury.Core.CharacterBuilder.Spellcasting;
-using Dawnsbury.Core.CombatActions;
-using Dawnsbury.Core.Creatures;
-using Dawnsbury.Core.Mechanics;
+﻿using Dawnsbury.Core.CharacterBuilder.Spellcasting;
 using Dawnsbury.Core.Mechanics.Enumerations;
 using Dawnsbury.Modding;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Dawnsbury.Mods.Remaster.HideLegacySpells
@@ -17,42 +12,17 @@ namespace Dawnsbury.Mods.Remaster.HideLegacySpells
         public static void LoadMod()
         {
             // A list of legacy spell ids for each level (starting at 0)
-            SpellId[][] legacySpells = new[] {
-                new[] { SpellId.AcidSplash, SpellId.RayOfFrost, SpellId.ProduceFlame, SpellId.DisruptUndead, SpellId.ChillTouch },
-                new[] { SpellId.BurningHands, SpellId.ColorSpray, SpellId.MagicMissile, SpellId.MageArmor, SpellId.MagicWeapon, SpellId.TrueStrike, SpellId.ShockingGrasp },
-                new[] { SpellId.AcidArrow, SpellId.CalmEmotions, SpellId.FlamingSphere, SpellId.HideousLaughter, SpellId.ObscuringMist, SpellId.SoundBurst, SpellId.Barkskin, SpellId.SpiritualWeapon, SpellId.TouchOfIdiocy }
+            SpellId[] legacySpells = new[] {
+               SpellId.AcidSplash, SpellId.RayOfFrost, SpellId.ProduceFlame, SpellId.DisruptUndead, SpellId.ChillTouch,
+               SpellId.BurningHands, SpellId.ColorSpray, SpellId.MagicMissile, SpellId.MageArmor, SpellId.MagicWeapon, SpellId.TrueStrike, SpellId.ShockingGrasp,
+               SpellId.AcidArrow, SpellId.CalmEmotions, SpellId.FlamingSphere, SpellId.HideousLaughter, SpellId.ObscuringMist, SpellId.SoundBurst, SpellId.Barkskin, SpellId.SpiritualWeapon, SpellId.TouchOfIdiocy
             };
-            for (int i = 0; i < legacySpells.Length; i++)
+            ModManager.RegisterActionOnEachSpell((spell) =>
             {
-                foreach (var legacySpell in legacySpells[i])
+                if (legacySpells.Contains(spell.SpellId))
                 {
-                    HideLegacySpell(legacySpell, i);
+                    spell.Traits.Add(Trait.SpellCannotBeChosenInCharacterBuilder);
                 }
-            }
-        }
-
-        public static void HideLegacySpell(SpellId legacySpellId, int minimumSpellLevel)
-        {
-            // Make the legacy version of the spell inaccessable by removing the casting traditions
-            ModManager.ReplaceExistingSpell(legacySpellId, minimumSpellLevel, delegate (Creature? creature, int spellLevel, bool inCombat, SpellInformation spellInformation)
-            {
-                CombatAction? existingSpell = minimumSpellLevel switch
-                {
-                    0 => Cantrips.LoadModernSpell(legacySpellId, creature, spellLevel, inCombat, spellInformation),
-                    1 => Level1Spells.LoadModernSpell(legacySpellId, creature, spellLevel, inCombat, spellInformation),
-                    2 => Level2Spells.LoadModernSpell(legacySpellId, creature, spellLevel, inCombat, spellInformation),
-                    _ => null
-                } ?? throw new Exception("Invalid Spell: " + legacySpellId);
-                IEnumerable<Trait> filteredTraits = existingSpell.Traits.Where((trait) => trait switch
-                {
-                    Trait.Arcane => false,
-                    Trait.Divine => false,
-                    Trait.Occult => false,
-                    Trait.Primal => false,
-                    _ => true
-                });
-                existingSpell.Traits = new Traits(filteredTraits, existingSpell);
-                return existingSpell;
             });
         }
     }
