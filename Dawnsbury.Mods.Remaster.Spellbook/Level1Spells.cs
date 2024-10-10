91b976966a66e0c424406ac07c426ba98fbe5e14
Robin McCollum <robin.mccollum@gmail.com>
2024-10-09 22:46:40 -0700

    Added Leaden Steps spell

--- Dawnsbury.Mods.Remaster.Spellbook/Level1Spells.cs
+++ Dawnsbury.Mods.Remaster.Spellbook/Level1Spells.cs
@@ -69,7 +69,6 @@ namespace Dawnsbury.Mods.Remaster.Spellbook
         // ? Spirit Link
         // ? Thoughtful Gift
 
-        // Leaden Steps
         // Mud Pit
         // Noxious Vapors
         // Protector Tree (depending on difficulty)
@@ -369,6 +368,66 @@ namespace Dawnsbury.Mods.Remaster.Spellbook
                             Illustration = (Illustration)IllustrationName.GreaseTile,
                             TransformsTileIntoHazardousTerrain = true
                         });
+                    }
+                });
+            });
+
+            // Leaden Steps from PC2
+            RemasterSpells.RegisterNewSpell("LeadenSteps", 1, (spellId, spellcaster, spellLevel, inCombat, spellInformation) =>
+            {
+                return Spells.CreateModern(IllustrationName.ElementMetal, "Leaden Steps", [Trait.Concentrate, Trait.Manipulate, Trait.Metal, Trait.Morph, Trait.Arcane, Trait.Primal, RemasterSpells.Trait.Remaster],
+                    "You partially transform a foe’s feet into unwieldy slabs of metal, slowing their steps.", "The target attempts a Fortitude saving throw" +
+                        RemasterSpells.StripInitialWhitespace(S.FourDegreesOfSuccess("The target is unaffected.",
+                                           "The target is encumbered and has weakness " + S.HeightenedVariable(1 + spellLevel, 2) + " to electricity until the end of your next turn. The spell can’t be sustained.",
+                                           "The target is encumbered and has weakness " + S.HeightenedVariable(1 + spellLevel, 2) + " to electricity.",
+                                           "The target is encumbered and has weakness " + S.HeightenedVariable(2 + spellLevel, 3) + " to electricity.")) +
+                        S.HeightenText(spellLevel, 1, inCombat, "{b}Heightened (+1){/b} The weakness increasess by 2."),
+                    Target.Ranged(6), spellLevel, SpellSavingThrow.Basic(Defense.Fortitude))
+                .WithSoundEffect(SfxName.AncientDust)
+                .WithEffectOnEachTarget(async (spell, caster, target, result) =>
+                {
+                    if (result == CheckResult.CriticalSuccess)
+                    {
+                        return;
+                    }
+                    int weakness = 1 + spellLevel + ((result == CheckResult.CriticalFailure) ? 1 : 0);
+                    QEffect clumsyEffect = QEffect.Clumsy(1);
+                    QEffect speedEffect = new QEffect("Encumbered", "-10 foot penalty all your Speeds")
+                    {
+                        Illustration = IllustrationName.Rock,
+                        BonusToAllSpeeds = (_) => new Bonus(-2, BonusType.Untyped, spell.Name, false),
+                    };
+                    // NOTE: I should really have something that prevents multiple encumbered effects from applying.
+                    QEffect encumberedEffect = new QEffect("Encumbered", "You're clumsy 1 and take a -10 penalty to all your Speeds.")
+                    {
+                        CountsAsADebuff = true,
+                        WhenExpires = (qEffect) => qEffect.Owner.RemoveAllQEffects((other) => other == speedEffect || other == clumsyEffect)
+                    };
+                    QEffect weaknessEffect = QEffect.DamageWeakness(DamageKind.Electricity, weakness);
+                    QEffect mainEffect = new QEffect("affected by Leaden Steps", "Encumbered and weakness " + weakness + " to electricity.")
+                    {
+                        Illustration = IllustrationName.ElementMetal,
+                        BonusToAllSpeeds = (_) => new Bonus(-2, BonusType.Status, spell.Name, false),
+                        CountsAsADebuff = true,
+                        WhenExpires = (qEffect) => qEffect.Owner.RemoveAllQEffects((other) => other == encumberedEffect || other == weaknessEffect),
+                    };
+                    target.AddQEffect(clumsyEffect);
+                    target.AddQEffect(speedEffect);
+                    target.AddQEffect(encumberedEffect);
+                    target.AddQEffect(weaknessEffect);
+                    target.AddQEffect(mainEffect);
+                    if (result <= CheckResult.Failure)
+                    {
+                        caster.AddQEffect(QEffect.Sustaining(spell, mainEffect));
+                    }
+                    else
+                    {
+                        QEffect notSustaining = new QEffect("unsustainable", "")
+                        {
+                            CannotExpireThisTurn = true,
+                            WhenExpires = (qEffect) => target.RemoveAllQEffects((other) => other == mainEffect)
+                        }.WithExpirationAtEndOfOwnerTurn();
+                        caster.AddQEffect(notSustaining);
                     }
                 });
             });
